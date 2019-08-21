1dc52da203402b970b07aaea1993852d68daa244
andyborrell <andyborrell@gmail.com>
2019-08-21 20:28:35 +0100

    Make index file default

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,10 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 source_suffix = ['.rst', '.txt']
+
+master_doc = 'index'
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
